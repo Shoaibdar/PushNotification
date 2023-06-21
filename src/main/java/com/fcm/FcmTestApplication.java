ed08104e2c0efebefe81034599b4ba70e159827b
Shoaib Shabir <shoaib.shabir@olamnet.com>
2023-06-21 16:55:00 +0530

    first commit

--- src/main/java/com/fcm/FcmTestApplication.java
+++ src/main/java/com/fcm/FcmTestApplication.java
@@ -2,78 +2,12 @@ package com.fcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class FcmTestApplication {
-	
-	static String postUrl = "https://fcm.googleapis.com/fcm/send";
-	static String token = "dTgMt10wFr-gL2YDjeTjVA:APA91bG8SM2VW3qPFkrMR3mq4xkIfFdnx-TWT0WhfcJVp7cHeX9O8j0qUnHY9FNE8wYDNCM6d7wMbtei7ZXYQI6vE5T_WaC1zpuNZpeiC5-ZzFpKfaeRjkZuL9uae8dvDnicrCtoawsx";
 
 	public static void main(String[] args) {
 		SpringApplication.run(FcmTestApplication.class, args);
-		
-		try {
-			RestTemplate restTemplate = new RestTemplate();
-			HttpHeaders headers = new HttpHeaders();
-			headers.set("Authorization", "key=AAAAZtmE6_Y:APA91bHXqVeMomsY5XghcDTv1d2NsUFjSQvavUjnG81seltbOB5WX8kbSSUH9pqgjGoYIj6tDjdPMluxog7lczehVDBA-tpQfApEZ-GAnXSxsLYZiRtpzrWUspxqjUh2zFcDp5nLRcnS");
-			headers.setContentType(MediaType.APPLICATION_JSON);
-	
-			Data body = new Data(token, "title", "body");
-
-			String json = new ObjectMapper().writeValueAsString(body);
-			HttpEntity<String> entity = new HttpEntity<>(json, headers);
-
-			Object res = restTemplate.exchange(postUrl, HttpMethod.POST, entity, Object.class);
-			System.out.println(res);
-
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
 	}
 }
 
-class Data {
-
-	String token;
-	String title;
-	String body;
-	
-	public String getToken() {
-		return token;
-	}
-	
-	public void setToken(String token) {
-		this.token = token;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public String getBody() {
-		return body;
-	}
-	
-	public void setBody(String body) {
-		this.body = body;
-	}
-	
-	public Data(String token, String title, String body) {
-		super();
-		this.token = token;
-		this.title = title;
-		this.body = body;
-	}
-
-}
---
+++ src/main/java/com/fcm/NotificationController.java
@@ -0,0 +1,60 @@
+package com.fcm;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class NotificationController {
+	
+	static String postUrl = "https://fcm.googleapis.com/fcm/send";
+	//static String token = "dTgMt10wFr-gL2YDjeTjVA:APA91bG8SM2VW3qPFkrMR3mq4xkIfFdnx-TWT0WhfcJVp7cHeX9O8j0qUnHY9FNE8wYDNCM6d7wMbtei7ZXYQI6vE5T_WaC1zpuNZpeiC5-ZzFpKfaeRjkZuL9uae8dvDnicrCtoawsx";
+
+	@GetMapping("/send{token}/{fcmServerKey}")
+	public void sendNotification(@PathVariable String token, @PathVariable String fcmServerKey) {
+
+		 // Set the FCM API endpoint
+        String fcmUrl = "https://fcm.googleapis.com/fcm/send";
+        
+        // Set the notification body
+        String notificationBody = "OTP for Keycloak login 358482";
+        
+        // Create the HTTP headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", fcmServerKey);
+        
+        // Create the request body
+        String requestBody = "{\n" +
+                "    \"to\": \"" + token + "\",\n" +
+                "    \"notification\": {\n" +
+                "        \"body\": \"" + notificationBody + "\",\n" +
+                "        \"title\": \"bKeycloak\",\n" +
+                "        \"subtitle\": \"Subtitile\"\n" +
+                "    }\n" +
+                "}";
+        
+        // Create the HTTP entity
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+        
+        // Create the RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+        
+        // Send the HTTP request
+        ResponseEntity<String> response = restTemplate.exchange(fcmUrl, HttpMethod.POST, entity, String.class);
+        
+        // Process the response
+        if (response.getStatusCode().is2xxSuccessful()) {
+            System.out.println("Notification sent successfully");
+        } else {
+            System.out.println("Failed to send notification. Response: " + response.getBody());
+        }
+	}
+}
